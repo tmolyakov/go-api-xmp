2c34e6aab6478767daffb4468f6abc40b7bf8de9
Anatoliy Molyakov <tmolyakov@gmail.com>
2022-04-23 14:31:21 +0300

    response codes

--- internal/handlers/user/handler.go
+++ internal/handlers/user/handler.go
@@ -31,25 +31,31 @@ func (h *handler) Register(router *httprouter.Router) {
 }
 
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
 	w.Write([]byte("this is list of users"))
 }
 
 func (h *handler) GetByUuid(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
 	w.Write([]byte("this is requested user"))
 }
 
 func (h *handler) Create(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
 	w.Write([]byte("user created"))
 }
 
 func (h *handler) Update(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("user udpated"))
 }
 
 func (h *handler) PartiallyUpdate(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("user partially updated"))
 }
 
 func (h *handler) Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("user deleted"))
 }
