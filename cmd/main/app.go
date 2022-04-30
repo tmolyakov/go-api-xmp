59c37af37100d104cb43a0b532abe4c8c18627fa
Anatoliy Molyakov <tmolyakov@gmail.com>
2022-04-30 18:39:30 +0300

    sock

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/tmolyakov/go-api-xmp/internal/config"
 	"github.com/tmolyakov/go-api-xmp/internal/handlers/user"
 	"github.com/tmolyakov/go-api-xmp/pkg/logging"
 )
@@ -15,20 +20,40 @@ func main() {
 	logger.Info("create router")
 	router := httprouter.New()
 
+	cfg := config.GetConfig()
+
 	logger.Info("register user handler")
 	handler := user.NewHandler(logger)
 	handler.Register(router)
 
-	start(router)
+	start(router, cfg)
 }
 
-func start(router *httprouter.Router) {
+func start(router *httprouter.Router, cfg *config.Config) {
 	logger := logging.GetLogger()
 	logger.Info("starting application...")
-	listener, err := net.Listen("tcp", ":1234")
 
-	if err != nil {
-		panic(err)
+	var listener net.Listener
+	var listenErr error
+
+	if cfg.Listen.Type == "sock" {
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.Info("create socket")
+		socketPath := path.Join(appDir, "app.sock")
+
+		listener, listenErr = net.Listen("unix", socketPath)
+		logger.Infof("server is listening unix socket: %s", socketPath)
+	} else {
+		logger.Info("listen tcp")
+		listener, listenErr = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIp, cfg.Listen.Port))
+		logger.Infof("server is listening at %s:%s", cfg.Listen.BindIp, cfg.Listen.Port)
+	}
+
+	if listenErr != nil {
+		logger.Fatal(listenErr)
 	}
 
 	server := &http.Server{
@@ -36,8 +61,6 @@ func start(router *httprouter.Router) {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-
-	logger.Info("server is listening at 0.0.0.0:1234")
 
 	logger.Fatal(server.Serve(listener))
 }
