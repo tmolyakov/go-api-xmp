b784ed8a0db725cd74c48ba084e3e4ee38c381f0
Anatoliy Molyakov <tmolyakov@gmail.com>
2022-05-19 23:21:01 +0300

    mongodb client

---
+++ pkg/client/mongodb/mongodb.go
@@ -0,0 +1,43 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewClient(ctx context.Context, host, port, username, password, database, authDB string) (db *mongo.Database, err error) {
+	var MongoDBUrl string
+	var isAuth bool
+	if username == "" && password == "" {
+		MongoDBUrl = fmt.Sprintf("mongodb://%s:%s", host, port)
+	} else {
+		MongoDBUrl = fmt.Sprintf("mongodb://%s:%s@%s:%s", username, password, host, port)
+		isAuth = true
+	}
+
+	clientOptions := options.Client().ApplyURI(MongoDBUrl)
+	if isAuth {
+		if authDB == "" {
+			authDB = database
+		}
+		clientOptions.SetAuth(options.Credential{
+			AuthSource: authDB,
+			Username:   username,
+			Password:   password,
+		})
+	}
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to mongoDB due to error: %v", err)
+	}
+
+	if err = client.Ping(ctx, nil); err != nil {
+		return nil, fmt.Errorf("failed to ping mongoDB due to error: %v", err)
+	}
+
+	return client.Database(database), nil
+}
